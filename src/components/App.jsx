14b156ac1a87904970cde791425efa0f55902382
yewwoei <yewwoei.low@outlook.com>
2019-05-12 20:15:14 -0700

    add navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 /** Components */
 import BaseCanvas from "./BaseCanvas";
+import Navbar from "./Navbar";
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
+        <Navbar />
         <BaseCanvas />
       </div>
     );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import SystemTwoLogo from "../assets/logo.png";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="navbar">
+        <img src={SystemTwoLogo} className="navbar-logo" alt="logo" />
+        <h1 className="navbar-title">System Two</h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
