4142760fc79337e4f44e3bf24e4b4a08f562f2e4
yewwoei <yewwoei.low@outlook.com>
2019-07-29 23:27:15 -0700

    add integration with google app engine

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,13 @@ class Navbar extends React.Component {
   render() {
     return (
       <div className="navbar">
-        <img src={SystemTwoLogo} className="navbar-logo" alt="logo" />
-        <h1 className="navbar-title text--title">Determinae</h1>
+        {/* <img src={SystemTwoLogo} className="navbar-logo" alt="logo" /> */}
+        <div className="navbar__text">
+          <h1 className="navbar-title text--title">Determinae</h1>
+          <h5 className="navbar-subtitle">
+            Rational Decision Making with Decision Matrixes
+          </h5>
+        </div>
       </div>
     );
   }
